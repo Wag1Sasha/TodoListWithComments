54bb9249127dc4d1def5bd6d28095fb4c10a5213
Alex Hoienko <58471316+Wag1Sasha@users.noreply.github.com>
2020-07-06 23:05:31 +0300

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function App(): JSX.Element {
+  return (
+    <>
+      <div> Typescript</div>
+    </>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
